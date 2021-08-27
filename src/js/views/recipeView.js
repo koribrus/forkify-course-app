6924122bb8b79ab3dac7f627a351024b010720af
bruzky <i@koribrus.com>
2021-08-26 22:21:45 -0700

    replaced fractional module with formula

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -2,7 +2,8 @@ import View from './View';
 
 import icons from 'url:../../img/icons.svg';
 // import { Fraction } from 'fractional';
-const Fraction = require('fractional').Fraction;
+// import { numberToFraction } from './helpers.js';
+import { numberToFraction } from '../helpers.js';
 
 class RecipeView extends View {
   _parentEl = document.querySelector('.recipe');
@@ -131,7 +132,7 @@ class RecipeView extends View {
             <use href="${icons}#icon-check"></use>
           </svg>
           <div class="recipe__quantity">${
-            ing.quantity ? new Fraction(ing.quantity) : ''
+            ing.quantity ? numberToFraction(ing.quantity) : ''
           }</div>
           <div class="recipe__description">
           <span class="recipe__unit">${ing.unit}</span>
